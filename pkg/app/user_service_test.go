b889cf494e6af4441ca08fea2411c112c087d73c
Vinícius Crisol <vinicius.crisol@luizalabs.com>
2024-07-12 09:31:13 -0300

    test: add tests for user service

--- pkg/app/user_service_test.go
+++ pkg/app/user_service_test.go
@@ -45,6 +45,13 @@ func TestUserService_CreateUser(t *testing.T) {
 	)
 
 	t.Run(
+		"It should return an error when the user validation fails", func(t *testing.T) {
+			setup()
+			assert.Error(t, userService.CreateUser(CreateUserCmd{}))
+		},
+	)
+
+	t.Run(
 		"It should return an error when the user email is already in use", func(t *testing.T) {
 			setup()
 			mockedUserDAO.On("FindByEmail", email).Return(entity.User{}, true, nil)
@@ -93,6 +100,90 @@ func TestUserService_CreateUser(t *testing.T) {
 					},
 				),
 			)
+		},
+	)
+}
+
+func TestUserService_AuthenticateService(t *testing.T) {
+	email := "john.doe@email.com"
+	password := "123456"
+	user, _ := entity.NewUser("John Doe", email, password)
+
+	var (
+		mockedUserDAO *mocked.UserDAO
+		userService   *UserService
+	)
+
+	setup := func() {
+		mockedUserDAO = &mocked.UserDAO{}
+		userService = NewUserService(mockedUserDAO)
+	}
+
+	t.Run(
+		"It should authenticate a user successfully with a valid email/password combination", func(t *testing.T) {
+			setup()
+			mockedUserDAO.On("FindByEmail", email).Return(user, true, nil)
+
+			token, err := userService.AuthenticateUser(
+				AuthenticateUserCmd{
+					Email:    email,
+					Password: password,
+				},
+			)
+
+			assert.NoError(t, err)
+			assert.NotEmpty(t, token)
+		},
+	)
+
+	t.Run(
+		"It should return an error when the email does not match any user", func(t *testing.T) {
+			setup()
+			mockedUserDAO.On("FindByEmail", email).Return(entity.User{}, false, nil)
+
+			token, err := userService.AuthenticateUser(
+				AuthenticateUserCmd{
+					Email:    email,
+					Password: password,
+				},
+			)
+
+			assert.Equal(t, ErrAuthenticationFailed, err)
+			assert.Empty(t, token)
+		},
+	)
+
+	t.Run(
+		"It should return an error when the password does not match", func(t *testing.T) {
+			setup()
+			mockedUserDAO.On("FindByEmail", email).Return(user, true, nil)
+
+			token, err := userService.AuthenticateUser(
+				AuthenticateUserCmd{
+					Email:    email,
+					Password: "1234567",
+				},
+			)
+
+			assert.Equal(t, ErrAuthenticationFailed, err)
+			assert.Empty(t, token)
+		},
+	)
+
+	t.Run(
+		"It should return an error when FindByEmail method fails", func(t *testing.T) {
+			setup()
+			mockedUserDAO.On("FindByEmail", email).Return(entity.User{}, false, errors.New("some error"))
+
+			token, err := userService.AuthenticateUser(
+				AuthenticateUserCmd{
+					Email:    email,
+					Password: password,
+				},
+			)
+
+			assert.Error(t, err)
+			assert.Empty(t, token)
 		},
 	)
 }
